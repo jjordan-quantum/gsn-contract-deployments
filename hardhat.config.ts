e2499da79d7350010c417f86d14cf9b36230caba
jjfluid <constant-space@gmail.com>
2024-02-25 20:05:22 -0800

    added gsn contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: 'london',
+    },
+  },
 };
 
 export default config;
