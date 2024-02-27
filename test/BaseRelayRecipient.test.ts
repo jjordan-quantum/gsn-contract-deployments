321c938349cae2503ba1b9d42f23a315fe3e9f9c
jjfluid <constant-space@gmail.com>
2024-02-26 19:36:04 -0800

    added test case for _msgData

--- test/BaseRelayRecipient.test.ts
+++ test/BaseRelayRecipient.test.ts
@@ -1,24 +1,32 @@
 import { expect } from "chai";
-import { ethers} from "hardhat";
+import { ethers, artifacts} from "hardhat";
 import {Forwarder, TestForwarderTarget} from "../typechain-types";
+import {Artifact} from "hardhat/types";
+import {toHex} from "web3-utils";
+const abiCoder = new ethers.AbiCoder();
 
 describe('BaseRelayRecipient', async () => {
   let recipient: TestForwarderTarget;
   let fwd: Forwarder;
   let deployer: any;
+  let recipientArtifact: Artifact;
+  let iface: any;
 
   before(async () => {
     fwd = await ethers.deployContract("Forwarder", []);
     recipient = await ethers.deployContract("TestForwarderTarget", [fwd.target]);
     deployer = (await ethers.getSigners())[0];
+    recipientArtifact = await artifacts.readArtifact('TestForwarderTarget');
+    iface = new ethers.Interface(recipientArtifact.abi);
   });
 
   describe('#_msgSender', async ()  => {
     async function callMsgSender (from: string, appended = ''): Promise<any> {
       const encoded = (await recipient.publicMsgSender.populateTransaction()).data;
-      const ret = await ethers.provider.call({ from, to: recipient.target, data: encoded + appended.replace(/^0x/, '') })
+      const ret = await ethers.provider.call({ from, to: recipient.target, data: encoded + appended.replace(/^0x/, '') });
       return ('0x' + ret.slice(ret.length - 40, ret.length)).toLowerCase();
     }
+
     it('should leave from address as-is if not from trusted forwarder', async () => {
       expect(await recipient.publicMsgSender({from: deployer.address})).to.eql(deployer.address);
     });
@@ -38,29 +46,40 @@ describe('BaseRelayRecipient', async () => {
     });
   })
 
-  // describe('#_msgData', async function () {
-  //   const encoded = recipient.contract.methods.publicMsgData().encodeABI() as string
-  //
-  //   async function callMsgData (from: string, appended = ''): Promise<any> {
-  //     const ret = await web3.eth.call({
-  //       from,
-  //       to: recipient.address,
-  //       data: encoded + appended.replace(/^0x/, '')
-  //     })
-  //     return web3.eth.abi.decodeParameter('bytes', ret)
-  //   }
-  //
-  //   const extra = toHex('some extra data to add, which is longer than 20 bytes').slice(2)
-  //   assert.equal(await callMsgData(from), encoded, 'should leave msg.data as-is if not from trusted forwarder')
-  //   assert.equal(await callMsgData(from, extra), encoded + extra, 'should leave msg.data as-is if not from trusted forwarder')
-  //
-  //   assert.equal(await callMsgData(fwd.address), encoded, 'should leave msg.data as-is if not enough appended data')
-  //
-  //   const sender = '0x'.padEnd(42, '12')
-  //   assert.equal(await callMsgData(fwd.address, extra + sender.slice(2)), encoded + extra,
-  //     'should extract msg.data if called through trusted forwarder')
-  // })
-  //
+  describe('#_msgData', async function () {
+    async function callMsgData (from: string, appended = ''): Promise<any> {
+      const encoded = (await recipient.publicMsgData.populateTransaction()).data;
+      const ret = await ethers.provider.call({ from, to: recipient.target, data: encoded + appended.replace(/^0x/, '') });
+      const decoded = abiCoder.decode(['bytes'], ret);
+      return decoded[0];
+    }
+
+    //const extra = toHex('some extra data to add, which is longer than 20 bytes').slice(2);
+
+    it('should leave from msg.data as-is if not from trusted forwarder', async () => {
+      const encoded = (await recipient.publicMsgData.populateTransaction()).data;
+      expect(await recipient.publicMsgData()).to.eql(encoded);
+    });
+
+    it('should leave msg.data as-is if not from trusted forwarder', async () => {
+      const encoded = (await recipient.publicMsgData.populateTransaction()).data;
+      const extra = toHex('some extra data to add, which is longer than 20 bytes').slice(2);
+      expect(await callMsgData(String(deployer.address), extra)).to.eql(encoded + extra);
+    });
+
+    it('should leave msg.data as-is if not enough appended data', async () => {
+      const encoded = (await recipient.publicMsgData.populateTransaction()).data;
+      expect(await callMsgData(String(fwd.target))).to.eql(encoded);
+    });
+
+    it('should leave msg.data as-is if not enough appended data', async () => {
+      const sender = '0x'.padEnd(42, '12');
+      const extra = toHex('some extra data to add, which is longer than 20 bytes').slice(2);
+      const encoded = (await recipient.publicMsgData.populateTransaction()).data;
+      expect(await callMsgData(String(fwd.target), extra + sender.slice(2))).to.eql(encoded + extra);
+    });
+  });
+
   // it('should extract msgSender and msgData in transaction', async () => {
   //   // trust "from" as forwarder (using real forwarder requires signing
   //   const recipient = await TestForwarderTarget.new(from)
